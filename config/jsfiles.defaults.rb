d3c5b72774ea2f5277b2924d7ae24e40971bd414
Rico Sta. Cruz <rico@ambiescent.com>
2011-03-19 00:39:25 +0800

    Add underscore + backbone.

--- config/jsfiles.defaults.rb
+++ config/jsfiles.defaults.rb
@@ -1,4 +1,9 @@
 class Main
-  files = Dir[root(%w[public js {jquery,lib,app}.*.js])]
+  files  = Dir[root(%w[public js jquery.*.js])]
+  files += Dir[root(%w[public js underscore.js])]
+  files += Dir[root(%w[public js backbone.js])]
+  files += Dir[root(%w[public js lib.*.js])]
+  files += Dir[root(%w[public js app.js])]
+  files += Dir[root(%w[public js app.*.js])]
   set :js_files, JsFiles.new(files, :prefix => '/js/')
 end
