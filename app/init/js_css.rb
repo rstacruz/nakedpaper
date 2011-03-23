25fb3e10b6724b3603c24a72fa765775f9f9156b
Rico Sta. Cruz <rico@ambiescent.com>
2011-03-23 22:07:50 +0800

    Fix.

--- app/init/js_css.rb
+++ app/init/js_css.rb
@@ -6,7 +6,7 @@ class Main
   set :js_files, JsFiles.new(
     js_files_list,
     :prefix => '/js',
-    :file_prefix => root('./app/js'))
+    :file_prefix => root('app/js'))
 
   register Sinatra::JsSupport
   serve_js '/js', from: root('app/js')
